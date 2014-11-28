13d42adc6eae4d127663eee4debaafd30b320d6e
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2014-11-28 09:11:14 -0500

    Remove old sample folder

--- demos/NRSTC/canada/js/RAMP/Utils/decorator.js
+++
@@ -1,5 +0,0 @@
-/*! ramp-theme-canada 21-11-2014 12:57:38 : v. 4.0.0 
- * 
- * RAMP GIS viewer - Dragonfly; Sample of an implementation of RAMP with Canada.ca Theme 
- **/
-define([],function(){"use strict";return{getFindFcn:function(a,b){function c(c){return a(b,c)}return c}}});
--- demos/NRSTC/canada/js/RAMP/Utils/functionMangler.js
+++
@@ -1,5 +0,0 @@
-/*! ramp-theme-canada 21-11-2014 12:57:38 : v. 4.0.0 
- * 
- * RAMP GIS viewer - Dragonfly; Sample of an implementation of RAMP with Canada.ca Theme 
- **/
-define(["dojo/topic","dojo/_base/lang","dojo/on","utils/util"],function(a,b,c,d){"use strict";function e(){var e=c;c=function(a,c,f,g){return d.isUndefined(g)?e(a,c,f):e(a,c,b.hitch(g,f))};var f=a.subscribe;a.subscribe=function(a,b){return d.isUndefined(a)?void 0:f(a,b)}}return{load:function(a,b,c){e(),c()}}});
